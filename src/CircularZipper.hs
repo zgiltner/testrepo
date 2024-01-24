966a42e670e5786267aef0411bb79e5b9ff4cb9b
AJ Ruf <z0isch@gmail.com>
2024-01-24 16:31:06 -0500

    Oh baby

---
+++ src/CircularZipper.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module CircularZipper (
+    CircularZipper (..),
+    current,
+    right,
+    rights,
+    left,
+    lefts,
+    fromNonEmpty,
+    updateCurrent,
+    findRight,
+) where
+
+import Data.Coerce (coerce)
+import Data.List.NonEmpty (NonEmpty)
+import Data.List.NonEmpty.Zipper (Zipper)
+import qualified Data.List.NonEmpty.Zipper as Z
+import Data.Maybe (fromMaybe)
+
+-- | Zipper that ties the end back to the beginning
+newtype CircularZipper a = CircularZipper {getCircularZipper :: Zipper a}
+    deriving stock (Show)
+    deriving newtype (Foldable, Functor)
+
+instance Traversable CircularZipper where
+    traverse f (CircularZipper z) = CircularZipper <$> traverse f z
+
+current :: forall a. CircularZipper a -> a
+current = coerce @(Zipper a -> a) Z.current
+
+fromNonEmpty :: forall a. NonEmpty a -> CircularZipper a
+fromNonEmpty = coerce @(NonEmpty a -> Zipper a) Z.fromNonEmpty
+
+right :: CircularZipper a -> CircularZipper a
+right (CircularZipper z) = CircularZipper $ fromMaybe (Z.start z) $ Z.right z
+
+rights :: forall a. CircularZipper a -> [a]
+rights = coerce @(Zipper a -> [a]) Z.rights
+
+lefts :: forall a. CircularZipper a -> [a]
+lefts = coerce @(Zipper a -> [a]) Z.lefts
+
+left :: CircularZipper a -> CircularZipper a
+left (CircularZipper z) = CircularZipper $ fromMaybe (Z.end z) $ Z.left z
+
+findRight :: (a -> Bool) -> CircularZipper a -> Maybe (CircularZipper a)
+findRight f z = go 0 z
+  where
+    go n z'
+        | n == length z = Nothing
+        | otherwise =
+            let z'' = right z'
+             in if f $ current z'' then Just z'' else go (n + 1) z''
+
+updateCurrent :: (a -> a) -> CircularZipper a -> CircularZipper a
+updateCurrent f (CircularZipper z) = CircularZipper $ Z.replace (f $ Z.current z) z
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -6,40 +8,63 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE NoFieldSelectors #-}
 
 module Lib (app) where
 
+import CircularZipper (CircularZipper (..))
+import qualified CircularZipper as CZ
 import Control.Monad (replicateM_, unless, when)
 import Data.Foldable (for_, sequenceA_, traverse_)
 import Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet
 import Data.List.NonEmpty (NonEmpty ((:|)))
-import Data.List.NonEmpty.Zipper (Zipper)
-import qualified Data.List.NonEmpty.Zipper as Z
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.UUID as UUID
-import Game (GameState (..), Move (..), PlayerState (..), findInZipper, isGameOver, isPlayerAlive, isPlayerTurn, mkMove)
+import GHC.Generics (Generic)
+import Game (GameState (..), Move (..), PlayerState (..), isGameOver, isPlayerAlive, isPlayerTurn, mkMove)
 import Lucid hiding (for_)
+import Lucid.Htmx
 import Servant
 import Servant.HTML.Lucid
+import UpperCase (UpperCase (..))
+import qualified UpperCase as UpperCase
+import Web.FormUrlEncoded (FromForm (..))
 import WithPlayerApi (PlayerId (..))
 import qualified WithPlayerApi
 
-type API = Get '[HTML] (Html ())
+newtype GuessPost = GuessPost
+    { guess :: UpperCase
+    }
+    deriving stock (Generic)
+    deriving anyclass (FromForm)
+
+type API =
+    Get '[HTML] (Html ())
+        :<|> "gs" :> Capture "i" Int :> Get '[HTML] (Html ())
+        :<|> "guess" :> ReqBody '[FormUrlEncoded] GuessPost :> Post '[HTML] (Html ())
 
 api :: Proxy API
 api = Proxy
 
 server :: Maybe (Html ()) -> Server (WithPlayerApi.API API)
-server mHotReload = WithPlayerApi.withPlayerApi @API (allLinks api) $ home mHotReload
+server mHotReload = WithPlayerApi.withPlayerApi @API (allLinks (Proxy :: Proxy (Get '[HTML] (Html ())))) foo
+  where
+    foo playerId =
+        home mHotReload playerId (gs1, 0)
+            :<|> ( \i ->
+                    home mHotReload playerId (iterate (`mkMove` TimeUp) gs1 !! i, i)
+                 )
+            :<|> (\g -> home mHotReload playerId (mkMove gs1 (Guess g.guess), 0))
 
 app :: Maybe (Html ()) -> Application
 app = serve (Proxy :: (Proxy (WithPlayerApi.API API))) . server
 
-home :: Maybe (Html ()) -> PlayerId -> Handler (Html ())
-home mHotreload playerId = pure $ html_ $ do
+home :: Maybe (Html ()) -> PlayerId -> (GameState, Int) -> Handler (Html ())
+home mHotreload playerId (gs, i) = pure $ html_ $ do
     head_ $ do
         meta_ [charset_ "UTF-8"]
         meta_ [name_ "viewport_", content_ "width=device-width, initial-scale=1.0"]
@@ -47,60 +72,73 @@ home mHotreload playerId = pure $ html_ $ do
         script_ [src_ "https://unpkg.com/htmx.org@1.9.10", integrity_ "sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC", crossorigin_ "anonymous"] ("" :: String)
         title_ "Bombparty"
         sequenceA_ mHotreload
-    body_ $ do
-        h1_ $ toHtml $ UUID.toText $ getPlayerId playerId
-        div_ [id_ "container", class_ "container mx-auto px-4"] $ do
-            for_ [gs1, gs2, gs3, gs4] $ \gs -> do
-                br_ []
-                br_ []
-                br_ []
-                gameStateUI playerId gs
+    body_
+        $ div_
+            ( [ id_ "gameContainer"
+              , hxGet_ ("/gs/" <> T.pack (show $ i + 1))
+              , hxSwap_ "outerHTML"
+              ]
+                <> [hxTrigger_ "load delay:10s" | not $ isGameOver gs]
+            )
+        $ do
+            h1_ $ toHtml $ UUID.toText $ getPlayerId playerId
+            div_
+                [ class_ "container mx-auto px-4"
+                ]
+                $ gameStateUI playerId gs
 
 gameStateUI :: PlayerId -> GameState -> Html ()
 gameStateUI pId gs = do
     when (isGameOver gs) $ h1_ "GAME OVER"
-    unless (isGameOver gs) $ currentStringUI gs.currentString
+    unless (isGameOver gs) $ div_ [class_ "text-2xl font-mono"] $ toHtml gs.currentString
     ul_ [class_ "space-y-3"] $ do
-        traverse_ (playerStateUI gs) $ playerFirst pId gs.players
+        traverse_ (playerStateUI pId gs) $ playerFirst pId gs.players
 
-playerStateUI :: GameState -> PlayerState -> Html ()
-playerStateUI gs ps = li_ [class_ $ "p-2 rounded-lg " <> bg <> " " <> outline] $ do
+playerStateUI :: PlayerId -> GameState -> PlayerState -> Html ()
+playerStateUI pId gs ps = li_ [class_ $ "p-2 rounded-lg " <> bg <> " " <> outline] $ do
     h1_ $ toHtml $ UUID.toText $ getPlayerId ps.id
     letterUI ps.letters
-    livesUI ps.lives
+    div_ $ replicateM_ ps.lives $ toHtml ("❤️" :: String)
+    when (isPlayerAlive ps) $
+        if isGameOver gs
+            then h1_ "WINNER"
+            else
+                when (isPlayerAlive ps) $
+                    input_
+                        ( [ id_ $ "input-" <> UUID.toText (getPlayerId ps.id)
+                          , name_ "guess"
+                          , class_ $
+                                "border-2 "
+                                    <> if isActivePlayersTurn
+                                        then ""
+                                        else "bg-neutral-300"
+                          , hxPost_ "/guess"
+                          , hxTarget_ "#gameContainer"
+                          ]
+                            <> [disabled_ "" | not isActivePlayersTurn]
+                            <> [autofocus_ | isActivePlayersTurn]
+                        )
   where
+    isActivePlayersTurn = pId == ps.id && isPlayerTurn gs.players ps
     outline = if isPlayerTurn gs.players ps then "border-4 border-indigo-700" else "border-2"
     bg = if isPlayerAlive ps then "" else "bg-neutral-300"
 
-livesUI :: Int -> Html ()
-livesUI l = div_ $ replicateM_ l $ toHtml ("❤️" :: String)
-
-currentStringUI :: Text -> Html ()
-currentStringUI = div_ [class_ "text-2xl font-mono"] . toHtml
-
 letterUI :: HashSet Char -> Html ()
-letterUI ls = for_ ['a' .. 'z'] $ \l -> do
+letterUI ls = for_ ['A' .. 'Z'] $ \l -> do
     let weight = if l `HashSet.member` ls then "font-extrabold" else "font-extralight"
     span_ [class_ $ "tracking-widest " <> weight] $ toHtml [l]
 
-playerFirst :: PlayerId -> Zipper PlayerState -> [PlayerState]
-playerFirst pId = flatten . findInZipper ((== pId) . (.id))
+playerFirst :: PlayerId -> CircularZipper PlayerState -> [PlayerState]
+playerFirst pId cz = CZ.current playerCurrent : CZ.rights playerCurrent <> CZ.lefts playerCurrent
   where
-    flatten :: Zipper a -> [a]
-    flatten z = Z.current z : Z.rights z <> Z.lefts z
+    playerCurrent = fromMaybe cz $ CZ.findRight ((== pId) . (.id)) cz
 
 gs1 :: GameState
-gs1 = GameState (Z.fromNonEmpty $ ps1 :| [ps2, ps3]) "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG" mempty
-gs2 :: GameState
-gs2 = mkMove gs1 TimeUp
-gs3 :: GameState
-gs3 = mkMove gs2 TimeUp
-gs4 :: GameState
-gs4 = mkMove gs3 TimeUp
+gs1 = GameState (CZ.fromNonEmpty $ ps1 :| [ps2, ps3]) (UpperCase.fromText "FRI") mempty
 
 ps1 :: PlayerState
 ps1 = PlayerState (PlayerId $ fromJust $ UUID.fromString "87c1c582-29a8-47bd-991d-ed07cc4f1e38") mempty 2
 ps2 :: PlayerState
-ps2 = PlayerState (PlayerId $ fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000") mempty 2
+ps2 = PlayerState (PlayerId $ fromJust $ UUID.fromString "550e8400-e29b-41d4-a716-446655440000") mempty 1
 ps3 :: PlayerState
 ps3 = PlayerState (PlayerId $ fromJust $ UUID.fromString "112640ef-5e44-4be2-8379-346ab4c66acb") mempty 2
