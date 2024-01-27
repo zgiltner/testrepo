d048b3f52516719659155bd951ed6825dfa17bd5
AJ Ruf <z0isch@gmail.com>
2024-01-27 17:00:46 -0500

    Get main working

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,26 @@
 module Main (main) where
 
-import Lib (app)
+import RIO
+
+import App (App (..))
+import Game (GameState (..), initialGameState)
 import Network.Wai.Handler.Warp
+import qualified RIO.HashSet as HashSet
+import Server (app)
+import System.Random (mkStdGen)
 
 main :: IO ()
-main = run 8080 $ app Nothing
+main = do
+    wsGameState <- do
+        let s =
+                GameStateUnStarted
+                    $ initialGameState
+                        (mkStdGen 0)
+                        (HashSet.fromList ["the", "quick", "brown", "fox", "friday"])
+                        ["fri", "day"]
+        chan <- newTChanIO
+        newTVarIO (s, chan)
+
+    wsGameStateTimer <- newTVarIO Nothing
+
+    run 8080 $ app App{..} Nothing
